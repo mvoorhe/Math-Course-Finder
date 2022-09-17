02de224ff310911de44cad881920535b2cdd8506
mnvoo <mvoorhe@purdue.edu>
2022-09-17 16:41:00 -0400

    Added web-scrapping to find table row(s) of classes by taught user-inputed professor.

--- BS4_Main.py
+++ BS4_Main.py
@@ -12,7 +12,6 @@ import requests
 from bs4 import BeautifulSoup
 
 main_page = requests.get("https://www.math.purdue.edu/academic/courses/")
-
 keyword = input("What math class are you looking for (5 digit number)?\n")
 
 src = main_page.content
@@ -24,25 +23,28 @@ for link in links:
         class_url = link.attrs['href']
         # print(class_url)
 
+
 class_page = requests.get("https://www.math.purdue.edu/academic/courses/" + class_url)
 # print(class_page)
 
 src = class_page.content
 class_soup = BeautifulSoup(src, 'lxml')
 
-teacher_preference = input("Do you have a preference on who teaches the class?\n")
-teacher_preference.lower()
+teacher_preference = input("Do you have a preference on who teaches the class?\n").lower()
 if teacher_preference == "yes":
-    prof_name = input("What is the teacher's last name?\n")
+    prof_name = input("What is the teacher's last name?\n").capitalize()
+    print(prof_name)
 
-prof_links = class_soup.find_all("a")
-for prof_link in prof_links:
-    if prof_name in prof_link.text:
-        print(prof_link.parent.parent)
+prof_tags = class_soup.find_all("a")
+for prof_classes in prof_tags:
+    if prof_name in prof_classes.text:
+        print(prof_classes.parent.parent)
 
-day_preference = input("Do you care about what days the class is held on?\n")
-day_preference.lower()
+day_preference = input("Do you care about what days the class is held on?\n").lower()
 if day_preference == "yes":
-    day = input("What day(s) do you want (separate with commas)?\n")
+    days = input("What day(s) do you want (condtions for input)?\n").upper()
 
-
+day_tags = class_soup.find_all("td")
+for available_days in day_tags:
+    if days in available_days.text:
+        print(available_days.parent)
