ec104a2c4d9464a422dedcbbebae879ab70b371a
mnvoo <mvoorhe@purdue.edu>
2022-09-17 20:35:16 -0400

    Checks for user conditions and provides output accordingly

--- BS4_Main.py
+++ BS4_Main.py
@@ -11,6 +11,8 @@ Created: Saturday, September 17, 2022
 import requests
 from bs4 import BeautifulSoup
 
+# Finds appropriate math course according to user input and returns
+# new webpage to search through class information
 main_page = requests.get("https://www.math.purdue.edu/academic/courses/")
 keyword = input("What math class are you looking for (5 digit number)?\n")
 
@@ -21,30 +23,46 @@ links = soup.find_all("a")
 for link in links:
     if "MA " + keyword in link.text:
         class_url = link.attrs['href']
-        # print(class_url)
-
 
 class_page = requests.get("https://www.math.purdue.edu/academic/courses/" + class_url)
-# print(class_page)
-
 src = class_page.content
 class_soup = BeautifulSoup(src, 'lxml')
+
+
+# Finds class information for classes taught by
+# user-inputted teacher
+prof_name = "none"
 
 teacher_preference = input("Do you have a preference on who teaches the class?\n").lower()
 if teacher_preference == "yes":
     prof_name = input("What is the teacher's last name?\n").capitalize()
-    print(prof_name)
 
-prof_tags = class_soup.find_all("a")
-for prof_classes in prof_tags:
-    if prof_name in prof_classes.text:
-        print(prof_classes.parent.parent)
+# Finds class information for classes that occur
+# on specific days of the week
+days = "none"
 
 day_preference = input("Do you care about what days the class is held on?\n").lower()
 if day_preference == "yes":
-    days = input("What day(s) do you want (condtions for input)?\n").upper()
+    days = input("What day(s) do you want (single letter representation)?\n").upper()
 
-day_tags = class_soup.find_all("td")
-for available_days in day_tags:
-    if days in available_days.text:
-        print(available_days.parent)
+# Returning table data sets according
+# to user input
+
+if prof_name != "none":
+    prof_tags = class_soup.find_all("a")                            # checks if there was a professor input
+    for prof_classes in prof_tags:
+        if prof_name in prof_classes.text:
+            if days != "none":                                      # checks if there was a weekday input
+                for available_days in prof_classes.parent.parent:
+                    if days in available_days.text:
+                        print(available_days.parent)
+            else:
+                print(prof_classes.parent.parent)
+elif prof_name == "none" and days != "none":                        # checks if there was NOT a professor
+    day_tags = class_soup.find_all("td")
+    for available_days in day_tags:
+        if days in available_days.text:
+            print(available_days.parent)
+else:
+    table_rows = class_soup.find("table", {"class": "table table-condensed"})
+    print(table_rows)
